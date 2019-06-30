4f7aecd967711dbf48e748ff228aae6e2fb7502e
Leandro Barral <barral@ikecode.com.br>
2019-06-30 14:29:29 -0300

    feat: simple array sum challenge

---
+++ 003-simple-array-sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar, int arCount)
+    {
+        var sum = 0;
+
+        for (int i = 0; i < arCount; i++)
+        {
+            if (ar[i] <= 1000)
+            {
+                sum += ar[i];
+            }
+        }
+
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+        int result = simpleArraySum(ar, arCount);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
